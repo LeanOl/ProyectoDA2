de684ac2cc38a6b3556ce143e7a4c87ba957665a
luchosangui <luchosangui20@gmail.com>
2023-09-26 12:43:48 -0300

    product tiene sistema base de datos

---
+++ ClothingStore/Data/Concrete/ProductManagement.cs
@@ -0,0 +1,63 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Concrete
+{
+    public class ProductManagement : GenericRepository<Product> //trae de generic repository
+    {
+        
+       // private readonly DbContext _context; esto creo que no va
+        
+        public ProductManagement(DbContext context) 
+        {
+            Context = context;
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return Context.Set<Product>().ToList();
+        }
+
+        public Product GetProductById(int id)
+        {
+            return Context.Set<Product>().Find(id);//resolver
+        }
+
+        public void InsertProduct(Product product)
+        {
+            Context.Set<Product>().Add(product);
+            Context.SaveChanges();
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            Context.Set<Product>().Update(product);
+            Context.SaveChanges();
+        }
+
+        public void DeleteProduct(int id)
+        {
+            var product = GetProductById(id);
+            if (product != null)
+            {
+                Context.Set<Product>().Remove(product);
+                Context.SaveChanges();
+            }
+        }
+        public List<Product> GetProductosByBrand(string brand)
+        {
+            return Context.Set<Product>().Where(p => p.Brand == brand).ToList();
+        }
+
+        public List<Product> GetProductosByCategory(string category)
+        {
+            return Context.Set<Product>().Where(p => p.Category == category).ToList();
+        }
+
+    }
+}
---
+++ ClothingStore/Data/Interfaces/IProductManagement.cs
@@ -0,0 +1,24 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Interfaces
+{
+    internal interface IProductManagement
+    {
+        public interface IProductyManagment
+        {
+            IEnumerable<Product> GetAllProducts();
+            Product GetProductById(int id);
+            void InsertProduct(Product product);
+            void UpdateProduct(Product product);
+            void DeleteProduct(int id);
+            List<Product> GetProductByBrand(string brand);
+            List<Product> GetProductByCategory(string category);
+
+        }
+    }
+}
