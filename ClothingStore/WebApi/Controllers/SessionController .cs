77e5ebdd8c1ec1d4fd403dd982c5c62eca1f379d
facundorodriguez8 <facundorodriguez8@hotmail.com>
2023-10-05 17:34:14 -0300

    Se quitan los comentarios

--- ClothingStore/WebApi/Controllers/SessionController .cs
+++ ClothingStore/WebApi/Controllers/SessionController .cs
@@ -24,8 +24,8 @@ namespace WebApi.Controllers
             return Ok(new { token = token });
         }
 
-        [ServiceFilter(typeof(AuthenticationFilter))]
-        [AuthorizationFilter(RoleNeeded = "ADMIN")]
+        //[ServiceFilter(typeof(AuthenticationFilter))]
+        //[AuthorizationFilter(RoleNeeded = "ADMIN")]
         [HttpDelete]
         public IActionResult Logout([FromHeader] Guid Authorization)
         {
