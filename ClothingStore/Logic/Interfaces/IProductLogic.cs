5909f9db0592d14c44748f0ecf986194bc46b4ad
luchosangui <luchosangui20@gmail.com>
2023-10-04 23:47:30 -0300

    Product API/LOGIC

---
+++ ClothingStore/Logic/Interfaces/IProductLogic.cs
@@ -0,0 +1,15 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+
+namespace Logic.Interfaces
+{
+    public interface IProductLogic
+    {
+        Product CreateProduct(Product product);
+        IEnumerable<Product> GetAllProducts();
+        Product GetProductById(Guid id);
+        void UpdateProduct(Product product);
+        void DeleteProduct(Guid id);
+    }
+}
