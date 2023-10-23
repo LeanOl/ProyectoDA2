0bf7e990d70cbc00557e9dda8cf0804b07c392cf
LeanOl <olmedo.leandro8@gmail.com>
2023-10-22 23:42:29 -0300

    Green | filtar productos por marca y categoria test ok

--- ClothingStore/Tests/DataTests/ProductRepositoryTests.cs
+++ ClothingStore/Tests/DataTests/ProductRepositoryTests.cs
@@ -36,4 +36,36 @@ public class ProductRepositoryTests : ContextInMemory
         // Assert
         Assert.AreEqual(expected, result.First());
     }
+
+    [TestMethod]
+    public void GetFilteredBrandProduct_Ok()
+    {
+        // Arrange
+        var dbContext = createDbContext("GetFilteredBrandProduct");
+        var promotionManagement = new ProductManagement(dbContext);
+        var expected = new Product("Test Product 1", 10, "Test Description 1", "Test Brand 1", "Test Category 1", new List<ProductColor>());
+
+        dbContext.Set<Product>().Add(expected);
+        dbContext.SaveChanges();
+        // Act
+        var result = promotionManagement.GetFilteredProducts("Test Brand 1");
+        // Assert
+        Assert.AreEqual(expected, result.First());
+    }
+
+    [TestMethod]
+    public void GetFilteredCategoryProduct_Ok()
+    {
+        // Arrange
+        var dbContext = createDbContext("GetFilteredCategoryProduct");
+        var promotionManagement = new ProductManagement(dbContext);
+        var expected = new Product("Test Product 1", 10, "Test Description 1", "Test Brand 1", "Test Category 1", new List<ProductColor>());
+
+        dbContext.Set<Product>().Add(expected);
+        dbContext.SaveChanges();
+        // Act
+        var result = promotionManagement.GetFilteredProducts("Test Category 1");
+        // Assert
+        Assert.AreEqual(expected, result.First());
+    }
 }
