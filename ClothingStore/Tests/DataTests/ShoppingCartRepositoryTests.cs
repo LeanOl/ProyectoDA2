4b7a2f68ebe4d59f369e2e01f4b987c65b9b9a6a
LeanOl <olmedo.leandro8@gmail.com>
2023-11-02 01:28:19 -0300

    green | actualizar carrito repositorio test ok

---
+++ ClothingStore/Tests/DataTests/ShoppingCartRepositoryTests.cs
@@ -0,0 +1,45 @@
+﻿using Data;
+using Domain;
+
+namespace Tests.DataTests;
+[TestClass]
+public class ShoppingCartRepositoryTests : ContextInMemory
+{
+    [TestMethod]
+    public void UpdateShoppingCart_Ok() 
+    {
+        // Arrange
+        var dbContext = createDbContext("UpdateShoppingCart");
+        var shoppingCartManagement = new ShoppingCartManagement(dbContext);
+        ShoppingCart expected = new ShoppingCart()
+        {
+            UserId = Guid.NewGuid(),
+            IdCart = Guid.NewGuid(),
+            ShoppingCartProducts = new List<ShoppingCartProducts>()
+            {
+                new ShoppingCartProducts()
+                {
+                    ProductId = Guid.NewGuid(),
+                    Quantity = 1
+                }
+            }
+        };
+        dbContext.Set<ShoppingCart>().Add(expected);
+        dbContext.SaveChanges();
+
+        // Act
+        expected.ShoppingCartProducts.First().Quantity = 2;
+        expected.ShoppingCartProducts.Add(new ShoppingCartProducts()
+        {
+            ProductId = Guid.NewGuid(),
+            Quantity = 3
+        });
+        var result = shoppingCartManagement.Update(expected);
+
+        // Assert
+        CollectionAssert.AreEquivalent(expected.ShoppingCartProducts,result.ShoppingCartProducts);
+
+
+
+    }
+}
